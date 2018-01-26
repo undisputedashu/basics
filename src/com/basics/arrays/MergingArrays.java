2de6d8f84e74e2f8db32c10b1d3d5fafb25ac04f
ashu <ashutosh.4040@gmail>
2018-01-26 16:18:25 +0530

    Merge arrays.

---
+++ src/com/basics/arrays/MergingArrays.java
@@ -0,0 +1,39 @@
+package com.basics.arrays;
+
+public class MergingArrays {
+
+	private static final Integer MIN = Integer.MIN_VALUE;
+
+	public static void main(String args[]) {
+		int a[] = {2, 8, MIN, MIN, MIN, 13, MIN, 15, 20};//size m+n
+		int b[] = {5, 7, 9, 25};//size n
+		
+		moveToEnd(a);
+		merge(a, b);
+		for (int i=0;i<a.length;i++) System.out.println(a[i]);
+	}
+
+	private static void merge(int[] a, int[] b) {
+		int n = a.length, m = b.length;
+		int lb = 0, rb = m, tb = 0;
+		while (lb<m && rb<n) {
+			if (b[lb]<=a[rb]) a[tb++] = b[lb++];
+			else a[tb++] = a[rb++];
+		}
+		while (lb<m) a[tb++] = b[lb++];
+		while (rb<n) a[tb++] = a[rb++];
+	}
+
+	//Try to write this method yourself.
+	private static void moveToEnd(int[] a) {
+		int n = a.length;
+		int j = n-1;
+		for (int i=n-1;i>=0;i--) {
+			if (a[i] != MIN) {
+				a[j] = a[i];
+				j--;
+			}
+		}
+	}
+	
+}
