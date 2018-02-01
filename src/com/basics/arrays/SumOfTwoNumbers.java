de1c2795727476791330665c2b8d4d247fabef37
ashu <ashutosh.4040@gmail>
2018-02-01 21:57:20 +0530

    Bug fix.

--- src/com/basics/arrays/SumOfTwoNumbers.java
+++ src/com/basics/arrays/SumOfTwoNumbers.java
@@ -9,6 +9,7 @@ public class SumOfTwoNumbers {
 
 	public static void main(String args[]) {
 		int a[] = {4,1,5,3,2};
+		quick(a, 0, a.length-1);
 		int k = 9;
 		printNumbers(a, k);
 	}
@@ -28,4 +29,24 @@ public class SumOfTwoNumbers {
 		System.out.println("No such pair found whose sum is " + k);
 	}
 	
+	private static void quick(int[] a, int p, int q) {
+		if (p >= q) return;
+		int i = p, x = a[p];
+		for (int j=i+1;j<=q;j++) {
+			if (a[j]<=x) {
+				i++;
+				swap(a, i, j);
+			}
+		}
+		if (i != p) swap(a, i, p);
+		quick(a,p,i-1);
+		quick(a,i+1,q);
+	}
+
+	private static void swap(int[] a, int j, int i) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
 }
