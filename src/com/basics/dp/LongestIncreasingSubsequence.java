1a2976ab3044e9d342e3dc6e4077f95af0286837
ashu <ashutosh.4040@gmail>
2019-04-21 12:28:17 +0530

    add

--- src/com/basics/dp/LongestIncreasingSubsequence.java
+++ src/com/basics/dp/LongestIncreasingSubsequence.java
@@ -9,12 +9,12 @@ public class LongestIncreasingSubsequence {
 	private static int max = Integer.MIN_VALUE;
 	
 	public static void main(String args[]) {
-		int a[] = {1,3,5,4,2,3,4};
+		int a[] = {1,3,5,1};
 		int n = a.length;
 		int k = lisRec(a, n);
-		System.out.println("Recursive result=" + max);
+		System.out.println("Recursive result=" + k);
 		k = lisdp(a,n);
-		System.out.println("DP result=" + k);
+		System.out.println("DP result=" + max);
 	}
 
 	private static int lisRec(int[] a, int n) { 
@@ -30,6 +30,23 @@ public class LongestIncreasingSubsequence {
 		if (res > max) max = res;
 		return res;
 	}
+
+	//int a[] = {1,3,5,2,3};
+	//think why it will fail
+//	private static int lisRec(int[] a, int n) { 
+//		if (n == 1) return 1;
+//		
+//		int res = 1;
+//		for (int i=1;i<n;i++) {
+//			int curr = lisRec(a, i);
+//			if (a[n-1] > a[i-1] && curr+1 > res)
+//				res = curr + 1;
+//			else if (curr > res)
+//				res = curr;
+//		}
+//		
+//		return res;
+//	}
 	
 	private static int lisdp(int[] a, int n) {
 		int t[] = new int[n];
