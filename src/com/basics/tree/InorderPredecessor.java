392a4b17dde5b2c64a67cb23b80ffef09421a38e
undisputedashu <ashutosh.4040@gmail.com>
2019-11-19 01:19:24 -0800

    Adding tree questions

--- src/com/basics/tree/InorderPredecessor.java
+++ src/com/basics/tree/InorderPredecessor.java
@@ -1,5 +1,45 @@
 package com.basics.tree;
 
+import com.basics.tree.Tree.Node;
+
 public class InorderPredecessor {
+
+	private static Node root = new Node();
+    public static void main(String args[]) {
+        Tree t = new Tree();
+        t.add(root, 25);
+        t.add(root, 15);
+        t.add(root, 35);
+        t.add(root, 28);
+        t.add(root, 20);
+        t.add(root, 18);
+        t.trav(root);
+        System.out.println("**********************");
+        Node i = inp(root, 25);
+        if (i == null) System.out.println("No successor exist!!");
+        else System.out.println(i.data);
+    }
+    
+    private static Node inp(Node p, int d) {
+    	Node par = null;
+    	
+    	while (p != null && p.data != d) {
+    		if (d > p.data) {
+    			par = p;
+    			p = p.right;
+    		} else {
+    			p = p.left;
+    		}
+    	}
+    	
+    	if (p == null) return null;
+    	if (p.left != null) return max(p.left);
+    	return par;
+    }
+    
+    private static Node max(Node p) {
+    	while (p.right != null) p = p.right;
+    	return p;
+    }
 
 }
--- src/com/basics/tree/InorderSuccessor.java
+++ src/com/basics/tree/InorderSuccessor.java
@@ -22,11 +22,29 @@ public class InorderSuccessor {
         t.add(root, 18);
         t.trav(root);
         System.out.println("**********************");
-        Node i = ins(root, 35);
+        Node i = ins2(root, 20);
         if (i == null) System.out.println("No successor exist!!");
         else System.out.println(i.data);
     }
     
+	private static Node ins2(Node p, int d) {
+		Node par = null;
+		while (p != null && p.data != d) {
+			if (d < p.data) {
+				par = p;
+				p = p.left;
+			} else {
+				p = p.right;
+			}
+		}
+		
+		if (p == null) return null;
+		
+		if (p.right != null) return min(p.right);
+		
+		return par;
+	}
+
 	private static Node ins(Node p, int d) {
 		Node q = p, succ = null;
 		while (q != null) {
