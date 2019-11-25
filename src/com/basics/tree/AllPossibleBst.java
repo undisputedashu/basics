8f8a877b697925f96e85879cacd9dad7aaf06116
undisputedashu <ashutosh.4040@gmail.com>
2019-11-25 13:10:32 +0530

    minor clean up

--- src/com/basics/tree/AllPossibleBst.java
+++ src/com/basics/tree/AllPossibleBst.java
@@ -2,6 +2,7 @@ package com.basics.tree;
 
 import java.util.ArrayList;
 
+
 /**
  * https://www.geeksforgeeks.org/construct-all-possible-bsts-for-keys-1-to-n/
  * 
@@ -11,68 +12,50 @@ import java.util.ArrayList;
 public class AllPossibleBst {
 
 	public static void main(String args[]) {
-		ArrayList<Node> totalTreesFrom1toN = constructTrees(1, 3);
-		/* Printing preorder traversal of all constructed BSTs */
-		System.out.println("Preorder traversals of all constructed BSTs are :");
-		for (int i = 0; i < totalTreesFrom1toN.size(); i++) {
-			preorder(totalTreesFrom1toN.get(i));
+		ArrayList<Node> list = ct(1,3);
+		for (Node p : list) {
 			System.out.println();
+			trav(p);
 		}
 	}
 
-	private static void preorder(Node root) {
-		if (root != null) {
-			System.out.print(root.data + " ");
-			preorder(root.left);
-			preorder(root.right);
-		}
-	}
-
-	private static ArrayList<Node> constructTrees(int start, int end) {
-		ArrayList<Node> list = new ArrayList<>();
-		/*
-		 * if start > end then subtree will be empty so returning NULL in the list
-		 */
-		if (start > end) {
+	private static ArrayList<Node> ct(int s, int e) {
+		ArrayList<Node> list = new ArrayList<Node>();
+		
+		if (s > e) {
 			list.add(null);
 			return list;
 		}
-
-		/*
-		 * iterating through all values from start to end for constructing\ left and
-		 * right subtree recursively
-		 */
-		for (int i = start; i <= end; i++) {
-			/* constructing left subtree */
-			ArrayList<Node> leftSubtree = constructTrees(start, i - 1);
-
-			/* constructing right subtree */
-			ArrayList<Node> rightSubtree = constructTrees(i + 1, end);
-
-			/*
-			 * now looping through all left and right subtrees and connecting them to ith
-			 * root below
-			 */
-			for (int j = 0; j < leftSubtree.size(); j++) {
-				Node left = leftSubtree.get(j);
-				for (int k = 0; k < rightSubtree.size(); k++) {
-					Node right = rightSubtree.get(k);
-					Node node = new Node(i); // making value i as root
-					node.left = left; // connect left subtree
-					node.right = right; // connect right subtree
-					list.add(node); // add this tree to list
+		
+		for (int i=s;i<=e;i++) {
+			ArrayList<Node> lefts = ct(s,i-1);
+			ArrayList<Node> rights = ct(i+1,e);
+			
+			for (Node left: lefts) {
+				for (Node right:rights) {
+					Node root = new Node(i);
+					root.left = left;
+					root.right = right;
+					list.add(root);
 				}
 			}
 		}
 		return list;
 	}
-
+	
+	private static void trav(Node p) {
+		if (p != null) {
+			System.out.print(p.data + "  ");
+			trav(p.left);
+			trav(p.right);
+		}
+	}
+	
 	private static class Node {
-		Integer data;
+		int data;
 		Node left, right;
-		private Node() {}
-		private Node(int d) {
-			this.data = d;
+		private Node(int data) {
+			this.data = data;
 		}
 	}
 
