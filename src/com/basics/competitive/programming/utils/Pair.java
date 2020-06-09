28c868054165fbd462d3b4fd6e48a9b8664f2fc0
undisputedashu <ashutosh.4040@gmail.com>
2020-06-10 00:20:40 +0530

    add cp utils

---
+++ src/com/basics/competitive/programming/utils/Pair.java
@@ -0,0 +1,49 @@
+package com.basics.competitive.programming.utils;
+
+import java.util.Comparator;
+/**
+ * Utility for pair, coordinate and edges
+ */
+public class Pair {
+	Integer x;
+	Integer y;
+	private Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || !(o instanceof Pair))
+			return false;
+		Pair cor = (Pair)o;
+		return x.equals(cor.x) && y.equals(cor.y);
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = result + 31*x;
+		result = result + 31*y;
+		return result;
+	}
+	
+	static class PairComparatorX implements Comparator<Pair> {
+
+		@Override
+		public int compare(Pair o1, Pair o2) {
+			return o1.x.compareTo(o2.x);
+		}
+		
+	}
+
+	static class PairComparatorY implements Comparator<Pair> {
+
+		@Override
+		public int compare(Pair o1, Pair o2) {
+			return o1.y.compareTo(o2.y);
+		}
+		
+	}
+
+}
