af2da8b33cc90324710dfe87800315c269756bc9
undisputedashu <ashutosh.4040@gmail.com>
2020-07-29 18:08:20 +0530

    fix bug

--- src/com/basics/arrays/StockSellMultiple.java
+++ src/com/basics/arrays/StockSellMultiple.java
@@ -1,61 +1,54 @@
 package com.basics.arrays;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
 import java.util.Map.Entry;
 
 /**
- * https://www.geeksforgeeks.org/stock-buy-sell/
+ * https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0
  * @author asasjha
  *
  */
 public class StockSellMultiple {
 
-    public static void main(String args[]) {
-        int a[] = {23,13,25,29,33,19,34,45,65,67};
-        printMaxProfit(a);
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-->0) {
+			int n = in.nextInt();
+			int a[] = new int[n];
+			for (int i=0;i<n;i++) a[i] = in.nextInt();
+			maxProfit(a, n);
+		}
+		in.close();
+	}
 
-	private static void printMaxProfit(int[] a) {
-        int maxDiff = 0;
-        int minIndex = 0, sellIndex = 0;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        
-        for (int i=1;i<a.length;i++) {
-            int diff = a[i] - a[minIndex];
-            if (diff > maxDiff) {
-                maxDiff = diff;
-                sellIndex = i;
-                map.put(minIndex, sellIndex);
-            }
-            if (a[i] < a[minIndex]) {
-                minIndex = i;
-                maxDiff = 0;
-            }
-        }
-        
-        for (Entry<Integer, Integer> e:map.entrySet()) {
-            System.out.println(e.getKey() + "  " + e.getValue());
-        }
-    }
-
-	private static void printMaxProfit2(int[] a) {
-		int n = a.length;
-		int maxDiff = 0, l = 0, r = 0;
-		for (int i=1;i<n;i++) {
-			int diff = a[i] - a[l];
-			if (diff > maxDiff) {
-				maxDiff = diff;
-				r = i;
-			} else if (a[i] < a[i-1]) {
-				if (maxDiff > 0) System.out.println(l + "  " + r);
-				l = i;
-				r = i;
-				maxDiff = 0;
-			}
+	private static void maxProfit(int[] a, int n) {
+		if (n <= 1) {
+			System.out.println(0);
+			return;
 		}
 		
-		if (l != r) System.out.println(l + "  " + r);
+		int s = 0, e = 1;
+		LinkedHashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
+		while (e<n) {
+			if (a[e]>a[s]) {
+				while (e+1<n && a[e]<a[e+1]) e++;
+				map.put(s, e);
+				s = e+1;
+			} else s = e;
+			e++;
+		}
+		
+		if (map.isEmpty()) System.out.println("No Profit");
+		else {
+			StringBuilder sb = new StringBuilder();
+			for (Entry<Integer, Integer> en:map.entrySet()) {
+				sb.append("(").append(en.getKey()).append(" ").append(en.getValue()).append(")").append(" ");
+			}
+			System.out.println(sb);
+		}
+		
 	}
     
 }
