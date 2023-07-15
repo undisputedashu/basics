d7abfd1797e822aec49daa37c35fe495095f1b42
Ashutosh Jha <undisputedashu@users.noreply.github.com>
2023-07-15 23:09:53 +0530

    Update MinimumSubArrayWithSumDivisibleByK.java

--- src/com/basics/arrays/MinimumSubArrayWithSumDivisibleByK.java
+++ src/com/basics/arrays/MinimumSubArrayWithSumDivisibleByK.java
@@ -33,9 +33,15 @@ public class MinimumSubArrayWithSumDivisibleByK {
 	
 	//O(n)
     private static int optimalSolution(int n, int k, int[] a) {
+	if (n == 0) return -1;
+        if (a[0] % k == 0) return 1;
+	
     	long t[] = new long[n];
     	t[0] = a[0];
-    	for (int i=1;i<n;i++) t[i] = t[i-1] + a[i];
+    	for (int i=1;i<n;i++) {
+            t[i] = a[i] + t[i-1];
+            if (t[i]%k == 0) min = Math.min(min, i+1);
+        }
     	
     	int min = Integer.MAX_VALUE;
     	Map<Long, Integer> map = new HashMap<Long, Integer>();
